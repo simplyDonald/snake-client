ed04ebd4ceac97da33eba705b7d37fa21ccc53e3
simplyDonald <donaldabuah@gmail.com>
2021-11-03 23:44:12 +0000

    fixed error with brackets in code

--- play.js
+++ play.js
@@ -3,17 +3,16 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
+    host:'165.227.47.243',
     port: 50541,
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
   conn.on('data',(data)=>{
-    console.log('You are Connected!!',data)
-  };
+    console.log(data);
+  })
   return conn;
-})
+};
 
 console.log("Connecting ...");
 connect();
