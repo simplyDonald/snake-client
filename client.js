3c8515d5990adb61f569af2677bc2a9697679d3d
simplyDonald <donaldabuah@gmail.com>
2021-11-04 00:31:43 +0000

    send name string to server

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net')
+const connect = function () {
+  const conn = net.createConnection({
+    host:'165.227.47.243',
+    port: 50541,
+  });
+  conn.on('connect',()=>{
+    console.log("Successfully connected to game server")
+    conn.write('Name: LOL');
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('data',(data)=>{
+    console.log(data);
+  })
+  return conn;
+};
+
+module.exports = {connect};
